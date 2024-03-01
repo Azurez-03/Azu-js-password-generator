2974085f915705d399914557d506e0ddd8893d15
Azure-317 <151602308+Azure-317@users.noreply.github.com>
2024-02-29 18:57:18 -0600

    Started pw Code and added code for PW length

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,19 @@ var upChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var loChars = "abcdefghigklmnopqrstuvwxyz";
 var numChars = "0123456789";
 var simChars = "~!@#$%^&*?";
+
+function generatePassword() {
+  var actualPassword = "";
+
+  alert("the following promts are criteria for generating password.");
+
+  var pwdLenght = prompt("Chose the lenght of pasword (Between 8 and 128 charachters).");
+  if (pwdLength != null) {
+    if (pwdLenght >=8 && pwdLenght <=128)
+    {
+      alert (" ")
+    }
+  }
+
+
+} ;
