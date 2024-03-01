5d929efeaa853314ceb12687a72b778f642eef85
Azure-317 <151602308+Azure-317@users.noreply.github.com>
2024-02-29 20:05:17 -0600

    chose pw characters

--- assets/script.js
+++ assets/script.js
@@ -21,18 +21,53 @@ var loChars = "abcdefghigklmnopqrstuvwxyz";
 var numChars = "0123456789";
 var simChars = "~!@#$%^&*?";
 
+//pw generating code bellow
 function generatePassword() {
   var actualPassword = "";
 
   alert("the following promts are criteria for generating password.");
-
+  //pw length
   var pwdLenght = prompt("Chose the lenght of pasword (Between 8 and 128 charachters).");
   if (pwdLength != null) {
-    if (pwdLenght >=8 && pwdLenght <=128)
-    {
-      alert (" ")
+    if (pwdLenght >= 8 && pwdLenght <= 128) {
+      alert("One of the following must be a choise lowercase, upercase, numeric, and/or special character must be confirmed.");
+
+      var chooser = false;
+      var charChoices = "";
+
+      var upCase = confirm("Do you wnat uper case charachters?");
+      if (upCase === true) {
+        chooser = true;
+        charChoices += upChars;
+      }
+
+      var loCase = confirm("Do you want lower case charachters?");
+      if (loCase === true) {
+        chooser = true;
+        charChoices += loChars;
+      }
+      var numCase = confirm("Do you want numeric charachters?");
+      if (numCase === true) {
+        chooser = true;
+        charChoices += numChars;
+      }
+
+      var simCase = confirm("Do you want special charachters?");
+      if (simCase === true) {
+        chooser = true;
+        charChoices += simChars;
+      }
+
+      if (chosser === false) {
+        alert("You must choose at least one of lowercase, uppercase, numeric, and /or special character type.");
+      } else {
+        for (i =1; i< pwdLength; i++){
+          actualPassword += charChoices[Math.floor(Math.random()*charChoices.length)];
+        };
+      };
+      }
     }
   }
 
 
-} ;
+};
